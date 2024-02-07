a046d68793f0012689d681f34fe0d630c5fa9602
agent_tim <agent_tim@mail.ru>
2024-02-07 09:50:36 +0500

    задача 1

---
+++ _001_two_sum/solution_1.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if target == nums[i] + nums[j]:
+                    return [i, j]
+
+
+nums = [3, 2, 4]
+target = 6
+solution = Solution()
+print(solution.twoSum(nums, target))
