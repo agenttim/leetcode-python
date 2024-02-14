cd6a98ea2fe3152ddd412c04bc2f4379905bd433
agent_tim <agent_tim@mail.ru>
2024-02-14 10:57:45 +0500

    задача 21

--- _021_merge_two_sorted_list/draft.py
+++ _021_merge_two_sorted_list/draft.py
@@ -1,3 +1,6 @@
+from _021_merge_two_sorted_list.solution_1 import Solution
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -16,8 +19,22 @@ list2 = ListNode(2, ListNode(4, ListNode(6)))
 
 
 # Выводим результат
-print("List 1:")
-print_linked_list(list1)
+# print("List 1:")
+# print_linked_list(list1)
+#
+# print("List 2:")
+# print_linked_list(list2)
 
-print("List 2:")
-print_linked_list(list2)
+# dummy = ListNode(0)
+# current = dummy
+#
+# if list1:
+#     print(list1.val)
+#     current.next = list1
+#
+# print_linked_list(current)
+
+solution = Solution()
+list3 = solution.mergeTwoLists(list1, list2)
+print_linked_list(list3)
+
